7625ce2249e5b0baac11d612ca2c44a55d371306
komugi8 <8448eito@gmail.com>
2023-10-24 18:00:24 +0900

    change theme color

--- app/src/main/java/com/example/wataritabi/MainActivity.kt
+++ app/src/main/java/com/example/wataritabi/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.wataritabi.navigation.NavigationScreen
@@ -18,7 +17,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     NavigationScreen()
                 }
