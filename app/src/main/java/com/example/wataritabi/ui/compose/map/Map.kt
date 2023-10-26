c730ad69ca8d5858cec0162faaee65a47caa88f3
komugi8 <8448eito@gmail.com>
2023-10-26 17:50:30 +0900

    add FAB

--- app/src/main/java/com/example/wataritabi/ui/compose/map/Map.kt
+++ app/src/main/java/com/example/wataritabi/ui/compose/map/Map.kt
@@ -1,9 +1,18 @@
 package com.example.wataritabi.ui.compose.map
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Edit
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -13,19 +22,33 @@ import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapScreen() {
-    val singapore = LatLng(1.35, 103.87)
+    val tokyoStation = LatLng(35.6809591, 139.7673068)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(tokyoStation, 10f)
     }
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
+    Box {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                state = MarkerState(position = tokyoStation),
+                title = "東京駅",
+                snippet = "Marker in Tokyo Station"
+            )
+        }
+        ExtendedFloatingActionButton(
+            onClick = {},
+            Modifier
+                .align(Alignment.BottomEnd)
+                .padding(
+                    end = 64.dp,
+                    bottom = 32.dp
+                ),
+        ) {
+            Icon(Icons.Rounded.Edit, null)
+            Text(text = "目的地の編集")
+        }
     }
 }
 
