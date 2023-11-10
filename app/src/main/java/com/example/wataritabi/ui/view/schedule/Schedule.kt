ecdadedd1087f2cdf80263a6911f218cb1da1348
komugi8 <8448eito@gmail.com>
2023-11-11 01:09:18 +0900

    add fab

--- app/src/main/java/com/example/wataritabi/ui/view/schedule/Schedule.kt
+++ app/src/main/java/com/example/wataritabi/ui/view/schedule/Schedule.kt
@@ -6,8 +6,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -33,28 +38,43 @@ fun ScheduleScreen(
 //        Event("4:00 PM", "新規プロジェクトの検討"),
 //        Event("6:00 PM", "帰宅")
 //    )
-    viewModel.addSchedule(
-        ScheduleModel(
-            0,
-            "hoge",
-            "2023/10/01",
-            "10:00",
-            "東京駅",
-            "東京駅",
-            "東京駅"
-        )
-    )
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "今日の日程",
-            style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier.padding(8.dp)
-        )
-        EventCards(events = scheduleList.value)
+//    viewModel.addSchedule(
+//        ScheduleModel(
+//            0,
+//            "hoge",
+//            "2023/10/01",
+//            "10:00",
+//            "東京駅",
+//            "東京駅",
+//            "東京駅"
+//        )
+//    )
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Add,
+                    contentDescription = "Add"
+                )
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "今日の日程",
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(8.dp)
+            )
+            EventCards(events = scheduleList.value)
+        }
     }
 }
 
